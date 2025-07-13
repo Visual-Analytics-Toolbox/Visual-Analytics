d3500a1c164e7f95ebf0e16ca544edc8d2fdfbc6
emma schlotter <emmacal@protonmail.com>
2025-07-13 16:54:51 +0200

    added admin models and serializer for tag models

--- django/annotation/serializers.py
+++ django/annotation/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
-from .models import Annotation
+from .models import Annotation,AnnotationTag
+from common.serializers import TagSerializer
+from common.models import Tag
 
 
 class AnnotationSerializer(serializers.ModelSerializer):
     color = serializers.SerializerMethodField()
     frame_number = serializers.CharField(read_only=True)
+    tags = serializers.SerializerMethodField()
 
     class Meta:
         model = Annotation
@@ -12,3 +15,8 @@ class AnnotationSerializer(serializers.ModelSerializer):
 
     def get_color(self, obj):
         return Annotation.Classes.get_color(obj.class_name)
+
+    def get_tags(self, obj):
+        # Get all Tag objects related to this annotation via AnnotationTag
+        tags = Tag.objects.filter(annotations__annotation=obj)
+        return TagSerializer(tags, many=True).data
--- django/image/serializers.py
+++ django/image/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import NaoImage
-
+from common.serializers import TagSerializer
+from common.models import Tag
 
 class ImageSerializer(serializers.ModelSerializer):
     frame_number = serializers.ReadOnlyField()
-
+    tags = serializers.SerializerMethodField()
     class Meta:
         model = NaoImage
         fields = "__all__"
+
+    def get_tags(self, obj):
+        tags = Tag.objects.filter(images__image=obj)
+        return TagSerializer(tags, many=True).data
