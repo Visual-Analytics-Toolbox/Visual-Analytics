b6fb9cb30cc5afe662265a7357cea5d1459b7409
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-07-29 16:08:11 +0200

    feat: make it possible to create an event from the frontend

--- frontend/shared/components/EventCard/EventCard.jsx
+++ frontend/shared/components/EventCard/EventCard.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styles from './EventCard.module.css';
 import robocup_img from '@shared/assets/robocup.jpeg';
 
-const EventCard = ({ event }) => {
+export const EventCard = ({ event }) => {
     const navigate = useNavigate();
 
     return (
@@ -23,4 +23,20 @@ const EventCard = ({ event }) => {
     );
 };
 
-export default EventCard;
+export const CreateEventCard = () => {
+    return (
+        <div className={styles.event_card}>
+            <div className={styles.event_header}>
+                <img src={robocup_img} alt="RoboCup Image" />
+            </div>
+            <div className={styles.event_content}>
+                <p className={styles.event_title}>
+                    Create Event
+                </p>
+            </div>
+            <div className={styles.event_footer}>
+                <progress className={styles.event_progressbar} value="40" max="100">40%</progress>
+            </div>
+        </div>
+    );
+};
--- frontend/shared/components/EventListView/EventListView.jsx
+++ frontend/shared/components/EventListView/EventListView.jsx
@@ -1,6 +1,6 @@
 
 import { useQuery } from '@tanstack/react-query';
-import EventCard from '@shared/components/EventCard/EventCard';
+import { EventCard, CreateEventCard } from '@shared/components/EventCard/EventCard';
 import SkeletonCard from '@shared/components/SkeletonCard/SkeletonCard';
 
 import styles from './EventListView.module.css';
@@ -39,15 +39,19 @@ const EventListView = () => {
       </div>
       <div className={styles.projects_section}>
         <div className={`${styles.project_boxes} ${styles.jsGridView}`}>
+
           {events.isLoading ? (
             <SkeletonCard />
           ) : events.data.length > 0 ? (
-            events.data.map((event) => (
-              <EventCard
-                event={event}
-                key={event.name}
-              ></EventCard>
-            ))
+            <>
+              {events.data.map((event) => (
+                <EventCard
+                  event={event}
+                  key={event.name}
+                ></EventCard>
+              ))}
+              <CreateEventCard />
+            </>
           ) : (
             <div className={styles.emptyState}>
               No events found
@@ -55,7 +59,7 @@ const EventListView = () => {
           )}
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
