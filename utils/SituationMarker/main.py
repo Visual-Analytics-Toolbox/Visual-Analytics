f6fbdb04579bed1114e2a3f774277428cf087605
emma schlotter <emmacal@protonmail.com>
2025-09-16 18:53:40 +0200

    situation tool without sending to db

--- utils/SituationMarker/main.py
+++ utils/SituationMarker/main.py
@@ -1,50 +1,12 @@
 import socket
-import logging
-from GameControlData import GameControlData
+from utils.GameControlData import GameControlData
+from utils.logging import get_logger
 from pynput import keyboard
 from vaapi.client import Vaapi
 import os
-
-
-
-def get_games():
-    response = client.games.list()
-    for game in response:
-        print(game)
-
-
-
-
-
-def get_logger(    
-        LOG_FORMAT     = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-        LOG_NAME       = '',
-        LOG_FILE_INFO  = 'situation.log',
-        LOG_FILE_ERROR = 'error.log'):
-
-    log           = logging.getLogger(LOG_NAME)
-    log_formatter = logging.Formatter(LOG_FORMAT)
-
-    # comment this to suppress console output
-    # stream_handler = logging.StreamHandler()
-    # stream_handler.setFormatter(log_formatter)
-    # log.addHandler(stream_handler)
-
-    file_handler_info = logging.FileHandler(LOG_FILE_INFO, mode='w')
-    file_handler_info.setFormatter(log_formatter)
-    file_handler_info.setLevel(logging.INFO)
-    log.addHandler(file_handler_info)
-
-    file_handler_error = logging.FileHandler(LOG_FILE_ERROR, mode='w')
-    file_handler_error.setFormatter(log_formatter)
-    file_handler_error.setLevel(logging.ERROR)
-    log.addHandler(file_handler_error)
-
-    log.setLevel(logging.INFO)
-
-    return log
-
-
+from queue import Queue
+import threading
+from iterfzf import iterfzf
 
 class GameController():
     """
@@ -63,8 +25,8 @@ class GameController():
         self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.__socket.bind(('', 3838))
         self.__socket.settimeout(1)  # in sec
-        #error messages in diffrent logs than the messages
         self.logger = get_logger()
+        
     def run(self):
         try:
             data, address = self.__socket.recvfrom(8192)
@@ -73,33 +35,69 @@ class GameController():
                 if self.__source is None or address[0] == self.__source:
                     message = GameControlData(data)
                     self.logger.info(message.secsRemaining)
+                    return message
         except Exception as e:
             self.logger.error(e)
+            return None
 
-
-class Listener():
-    def __init__(self,GameController):
-        self.GameController = GameController
-        with keyboard.Listener(
-        on_release=self.on_release) as listener:
-            listener.join()
-        
-    
-    def on_release(self,key, injected):
-    
-        if key == keyboard.KeyCode(char='h'):            
-            self.GameController.run()
-
-
-
-if __name__ == "__main__":
-    client = Vaapi(
+class SituationMarker():
+    def __init__(self,key:str='h'):
+        self.key = key
+        self.client = Vaapi(
         base_url=os.environ.get("VAT_API_URL"),
         api_key=os.environ.get("VAT_API_TOKEN"),
     )
+        self.queue = Queue()
+        self.controller = GameController()
+        
+        self.game = None
+        
+        self.menu()
+        self.start_threads()
+        
 
-    get_games()
-
-    a = GameController()
-    b = Listener(a)
-
+    def start_threads(self):
+        listener_thread = threading.Thread(target=self.key_listener,args=(self.queue,),daemon=True)
+        listener_thread.start()
+        game_thread = threading.Thread(target=self.game_listener,args=(self.queue,),daemon=True)
+        game_thread.start()
+        
+    def menu(self):
+        games = self.client.games.list()
+        
+        game_map = {f"{game.start_time}: {game.team1} vs {game.team2} {game.half}": game for game in games}
+        selected_str = iterfzf(game_map.keys())
+        if selected_str:
+            selected_game = game_map[selected_str]
+            self.game = selected_game.id    
+                
+    def key_listener(self,q):
+        def on_release(key):
+            if key == keyboard.KeyCode.from_char(self.key):            
+                q.put('msg')
+            elif key == keyboard.Key.esc:
+                quit()
+        with keyboard.Listener(
+        on_release=on_release,suppress=True) as listener:
+            listener.join()
+            
+    def game_listener(self,q):
+        while True:
+            msg = q.get()
+            if msg == 'msg':
+                message = self.controller.run()
+                if message:
+                    print(f'sending this time  {message.secsRemaining} to db')
+            q.task_done()
+                
+            
+if __name__ == "__main__":
+    m = SituationMarker()
+    try:
+        # Keep the main thread alive
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("Shutting down...")
+    
+    
