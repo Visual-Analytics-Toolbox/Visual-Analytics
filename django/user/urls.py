7c0ea4e5687e92ab6228c445d0314e4b0a9f9cbb
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-11-03 12:19:54 +0100

    fix: urls

--- django/user/urls.py
+++ django/user/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import LoginView, LogoutView,DummyView,CurrentUserViewSet
+from django.views.generic import RedirectView
+from .views import LoginView, LogoutView, DummyView, CurrentUserViewSet
 from rest_framework import routers
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/events")),
     path("login", LoginView, name="mylogin"),
     path("logout", LogoutView, name="mylogout"),
     path("signup", DummyView, name="signup"),
 ]
 
 router = routers.DefaultRouter()
-router.register("user",CurrentUserViewSet)
+router.register("user", CurrentUserViewSet)
 urlpatterns += router.urls
