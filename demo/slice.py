d82925687fb19e167bbeecdd1c2f7c811102f4be
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-11-12 13:49:33 +0100

    feat: move demos to sdk repo

--- demo/slice.py
+++
@@ -1,9 +0,0 @@
-from vaapi.client import Vaapi
-import os
-
-if __name__ == "__main__":
-    client = Vaapi(
-        base_url=os.environ.get("VAT_API_URL"),
-        api_key=os.environ.get("VAT_API_TOKEN"),
-    )
-    client.video.slice(game=59, start=10, end=20)
