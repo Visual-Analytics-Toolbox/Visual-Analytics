0141dc79589744c7ae8cf05c338a0a4226daea96
emma schlotter <emmacal@protonmail.com>
2025-12-06 18:25:48 +0100

    fix: users with keycloak admin role are also admin in django now

---
+++ django/user/signals.py
@@ -0,0 +1,29 @@
+from django.dispatch import receiver
+from allauth.socialaccount.signals import pre_social_login
+
+@receiver(pre_social_login)
+def sync_keycloak_roles(request, sociallogin, **kwargs):
+    if sociallogin.account.provider != 'keycloak':
+        return
+    
+    extra_data = sociallogin.account.extra_data["userinfo"]
+    roles = []
+
+    if 'realm_access' in extra_data and 'roles' in extra_data['realm_access']:
+        roles.extend(extra_data['realm_access']['roles'])
+
+    user = sociallogin.user
+    
+    is_admin_in_keycloak = 'admin' in roles
+
+    has_changed = False
+
+    if is_admin_in_keycloak:
+        if not user.is_staff or not user.is_superuser:
+            user.is_staff = True
+            user.is_superuser = True
+            has_changed = True
+
+    if has_changed and user.pk:
+        user.save()
+        
