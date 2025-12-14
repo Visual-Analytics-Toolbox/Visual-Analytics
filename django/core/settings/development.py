8c239c93f7814d586eca77e8e1a12aa709e30334
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-12-14 18:26:38 +0100

    chore: update django

--- django/core/settings/development.py
+++ django/core/settings/development.py
@@ -31,7 +31,6 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_RENDERER_CLASSES": (
         "drf_orjson_renderer.renderers.ORJSONRenderer",
-        # "rest_framework.renderers.BrowsableAPIRenderer",
         "core.utils.BrowsableAPIRendererWithoutForms",
     ),
 }
