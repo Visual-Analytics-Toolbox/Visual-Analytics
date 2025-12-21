90316053a6a921af2570a347a085ff0154c59337
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-12-21 19:57:02 +0100

    chore: move frontend code to seperate repo

--- frontend/electron/preload/index.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose a limited set of APIs to the renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Function to request loading a video by filename
-  getVideoByFilename: (filename) => ipcRenderer.invoke('get-predefined-video-path', filename),
-  get_value: (key) => ipcRenderer.invoke('get-config', key),
-  set_value: (key, value) => ipcRenderer.invoke('save-config', key, value),
-});
