1eada722e50b04e4b6767ce7ba78e28590de7336
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-12-21 19:52:05 +0100

    chore: move design folder to seperate repo

---
+++ demo/get_teams.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+import os
+from vaapi.client import Vaapi
+
+client = Vaapi(
+    base_url=os.environ.get("VAT_API_URL"),
+    api_key=os.environ.get("VAT_API_TOKEN"),
+)
+my_list = client.team.list()
+if my_list:
+    print(my_list)
+
+d = {team.id: team.name for team in my_list}
+
+print()
+print(d)
