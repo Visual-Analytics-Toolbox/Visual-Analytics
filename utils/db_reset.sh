bedb7f49a2b773486f964306c5137fed1e5f7ffe
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2026-03-22 20:20:14 +0100

    fix: db reset for postgres clients > 16 - you cant delete the user you are currently use to execute a command anymore

--- utils/db_reset.sh
+++ utils/db_reset.sh
@@ -5,12 +5,20 @@ pushd .
 cd /tmp
 
 psql -d postgres -c "DROP DATABASE IF EXISTS $VAT_POSTGRES_DB";
+psql -d postgres -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+
+# create user and db
+psql -d postgres -c "
+DO \$\$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$VAT_POSTGRES_USER') THEN
+        CREATE ROLE $VAT_POSTGRES_USER WITH LOGIN PASSWORD '$VAT_POSTGRES_PASS';
+    ELSE
+        ALTER ROLE $VAT_POSTGRES_USER WITH PASSWORD '$VAT_POSTGRES_PASS';
+    END IF;
+END
+\$\$;"
 psql -d postgres -c "CREATE DATABASE $VAT_POSTGRES_DB;"
-
-psql -d postgres -c "DROP OWNED BY $VAT_POSTGRES_USER;"
-psql -d postgres -c "DROP USER IF EXISTS $VAT_POSTGRES_USER"
-psql -d postgres -c "CREATE USER $VAT_POSTGRES_USER WITH PASSWORD '${VAT_POSTGRES_PASS}'"
-
 # set permissions
 psql -d postgres -c  "GRANT ALL ON DATABASE $VAT_POSTGRES_DB TO $VAT_POSTGRES_USER;"
 psql -d postgres -c  "GRANT ALL PRIVILEGES ON DATABASE $VAT_POSTGRES_DB TO $VAT_POSTGRES_USER;"
