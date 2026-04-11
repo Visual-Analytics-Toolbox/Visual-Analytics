3460d6d65487d2b6224c64d7e16dc8ea75c1bcf4
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2026-04-11 12:12:29 +0200

    chore: remove annotation models from vat - we use labelstudio directly for this

--- django/annotation/urls.py
+++ django/annotation/urls.py
@@ -5,11 +5,10 @@ from rest_framework import routers
 app_name = "annotation"
 
 urlpatterns = [
-    path("annotation-count/", views.AnnotationCount.as_view(), name="annotation-count"),
+    
 ]
 
 router = routers.DefaultRouter()
-router.register("annotations", views.AnnotationViewSet)
-router.register("validation", views.ValidationViewSet)
+
 
 urlpatterns += router.urls
