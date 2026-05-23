195713baa8fc4d821bdabdfb3825a40354dd18f5
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2026-05-23 19:09:07 +0200

    chore: add new pagination type

--- django/core/pagination.py
+++ django/core/pagination.py
@@ -1,6 +1,12 @@
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import CursorPagination
 
 
 class LargeResultsSetPagination(LimitOffsetPagination):
     default_limit = 100
     page_size_query_param = "page_size"
+
+class CursorPagination(CursorPagination):
+    page_size = 100
+    page_size_query_param = 'limit'
+    ordering = 'id'  # This automatically handles the fast, indexed ordering!
