63bfdf80f0c0f695c1bbc5e7b1c6e9fc28877e2a
emma schlotter <emmacal@protonmail.com>
2026-06-01 20:07:10 +0200

    chore: improve image endpoint docs

--- django/image/schema.py
+++ django/image/schema.py
@@ -1,4 +1,5 @@
 from drf_spectacular.utils import extend_schema, extend_schema_view
+from drf_spectacular.extensions import OpenApiSerializerExtension
 
 image_viewset_schema = extend_schema_view(
     list=extend_schema(summary="List Images", description="", tags=["Images"]),
@@ -11,3 +12,19 @@ image_viewset_schema = extend_schema_view(
     count=extend_schema(summary="Count Images", description="", tags=["Images"]),
     validate=extend_schema(summary="Validate Images", description="", tags=["Images"]),
 )
+
+class ImageSerializerExtension(OpenApiSerializerExtension):
+   
+    target_class = 'image.serializers.ImageReadSerializer'
+
+    def map_serializer(self, auto_schema, direction):
+        schema = auto_schema._map_serializer(self.target, direction, bypass_extensions=True)
+        
+        if 'properties' in schema and 'image_url' in schema['properties']:
+            schema['properties']['image_url']['description'] = (
+                "Relative path. \n\n"
+                "**PREFIX REQUIRED:** Prepend `https://logs.berlin-united.com/` "
+                "to this value to get the full image URL."
+            )
+            
+        return schema
