0361c381344cc49e24070a84417b0efc8e148f5f
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2026-06-17 20:55:29 +0200

    fix: admin for situations

--- django/annotation/admin.py
+++ django/annotation/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import RawGCSituation
+from unfold.admin import ModelAdmin
 
+class RawGCSituationAdmin(ModelAdmin):
+    list_display = ["uuid", "json_data"]
 
-admin.site.register(RawGCSituation)
+admin.site.register(RawGCSituation, RawGCSituationAdmin)
